55eb14c2d4571de5657d429c5027eca48bd11ebb
Hemani Katyal <hkatyal@vmware.com>
2022-09-29 11:23:22 +0530

    Initial Commit

---
+++ src/main/java/com/security/oauthlogin/CustomResponseErrorHandler.java
@@ -0,0 +1,14 @@
+package com.security.oauthlogin;
+
+import java.io.IOException;
+
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+
+public class CustomResponseErrorHandler extends OAuth2ErrorResponseErrorHandler {
+    public void handleError(ClientHttpResponse response) throws IOException {
+        System.out.printf("Exception occurred during CSP authentication. Csp-Request-Id: %s, StatusCode: %s",
+                response.getHeaders().getFirst("pragma"), response.getStatusCode());
+        super.handleError(response);
+    }
+}
